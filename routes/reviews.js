89e6158f3fc4cc71b6fd27dbe90a1a1efd35204d
unknown <neeraj.nerlekar@gmail.com>
2020-09-07 18:39:04 -0600

    add review to show page.

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-
-/* GET reviews index /posts/:id/reviews */
-router.get("/", (req, res, next) => {
-  res.send("index /posts/:id/reviews");
-});
+const { asyncErrorHandler } = require("../middleware");
+const {
+  reviewCreate,
+  reviewUpdate,
+  reviewDestroy,
+} = require("../controllers/reviews");
 
 /* POST reviews create /posts/:id/reviews */
-router.post("/", (req, res, next) => {
-  res.send(" CREATE /posts/:id/reviews");
-});
-
-/* GET reviews edit /posts/:id/reviews/:reviews_id/edit */
-router.get("/:reviews_id/edit", (req, res, next) => {
-  res.send("EDIT /posts/:id/reviews/:reviews_id/edit");
-});
+router.post("/", asyncErrorHandler(reviewCreate));
 
 /* PUT reviews update /posts/:id/reviews/:reviews_id */
 router.put("/:reviews_id", (req, res, next) => {
